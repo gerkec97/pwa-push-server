9862d13a1f593c0b490c8284f4b70391d78aa5f5
Gerrit Keck <gerrit.keck@globalgaming.com>
2021-01-06 12:56:32 +0100

    adding endpoints to server

--- server/index.js
+++ server/index.js
@@ -6,8 +6,22 @@ http.createServer( (request, response) => {
 
   // Enable CORS
   response.setHeader('Access-Control-Allow-Origin', '*')
+  const { url, method } = request
+  // response.end(`Hello from HTTP server - Updated ${request.url}`)
 
-  response.end('Hello from HTTP server - Updated')
-
+  if (method === 'POST' && url.match(/^\/subscribe\/?$/)) {
+    let body = []
+    request.on('data', chunk => body.push(chunk))
+    request.on('end', () => response.end('Subscribed'))
+  } else if (method === 'GET' && url.match(/^\/key\/?$/)) {
+    response.end('Public Key')
+  } else if (method === 'POST' && url.match(/^\/push\/?$/)) {
+    let body = []
+    request.on('data', chunk => body.push(chunk))
+    request.on('end', () => response.end('Push Sent'))
+  } else {
+    response.statusCode = 404
+    response.end(`nothing found for ${url} using ${method}`)
+  }
 // Start the Server
 }).listen( 3333, () => { console.log('Server Running') })
